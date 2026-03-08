cd0862203109666d89d37f624bedcdbe3249e751
IRIS <vickyadvance5@gmail.com>
2026-03-08 18:39:22 +0530

    Initialize frontend at root level

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import HealthLogin from './login.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <HealthLogin />
+    </React.StrictMode>,
+)
